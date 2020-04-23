8aeebb13b1290e926fbb29fe5a11d5b0344d391e
bcwan <bcwan8@gmail.com>
2020-04-23 14:49:32 -0700

    working redux state

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { postUser, deleteSession, postSession } from './util/session_api_util';
 import { usersReducer } from './reducers/users_reducer';
+import configureStore from './store/store';
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
 
   window.postUser = postUser;
   window.postSession = postSession; 
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,6 +3,7 @@ import { sessionsReducer } from './sessions_reducer';
 import errorsReducer from './errors_reducer';
 import entitiesReducer from './entities_reducer';
 
+
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   session: sessionsReducer,
